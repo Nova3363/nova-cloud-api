7c7ae7be54fc154ec50fd40c8e5fe37e6fc1b324
Nova3363 <aser3363@gmail.com>
2025-11-18 12:22:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Nova Cloud API running'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
