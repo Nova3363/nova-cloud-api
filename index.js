19be412e66c0171306fa06800b1dfaa7bfd88387
Nova3363 <aser3363@gmail.com>
2025-11-18 12:36:02 +0300

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Nova Cloud API running'));
-app.listen(process.env.PORT || 3000, () => console.log('Server started'));
